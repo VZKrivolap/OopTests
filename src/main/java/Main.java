78d04ab16e9b3f342917052b2331ac2e8b8d545a
VZKrivolap <vz984762@gmail.com>
2023-12-12 00:39:29 +0300

    24

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,10 @@
 import Task.*;
 
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-//        NoteBook noteBook = new NoteBook(500, "Linux", 16, 5000, false, 100, "Asus");
-//        PCMaster pcMaster = new PCMaster();
-//        try {
-//            pcMaster.turnOnOrOff(noteBook);
-//        } catch (MasterExeption e) {
-//            throw new RuntimeException(e);
-//        }
-
+    public static void main(String[] args) throws IOException {
         Device device1 = new Computer(500, "Windows", 500, 1000, true);
         Device device2 = new NetBook(500, "Windows", 400, 1500, true, 100, "Asus");
         Device device3 = new NoteBook(300, "Linux", 256, 2000, false, 120, "LG");
@@ -20,31 +13,9 @@ public class Main {
         Person person2 = new Person(device2, 10000);
         Person person3 = new Person(device3, 6000);
 
-//        List<Person> personList = new ArrayList<>();
-//        personList.add(person1);
-//        personList.add(person2);
-//        personList.add(person3);
-//
-//        person1.setName("Ivan");
-//        person2.setName("Ivan");
-//        person3.setName("Petr");
-//
-//        Set<String> personSet = new HashSet<>();
-//        personSet.add(person1.getName());
-//        personSet.add(person2.getName());
-//        personSet.add(person3.getName());
-//
-//        System.out.println(personSet);
-//
-//        person1.setName("Ivan");
-//        person2.setName("Oleg");
-//        person3.setName("Petr");
-//
-//        HashMap<String, Device> hashMap = new HashMap<>();
-//        hashMap.put(person1.getName(), person1.getDevice());
-//        hashMap.put(person2.getName(), person2.getDevice());
-//        hashMap.put(person3.getName(), person3.getDevice());
-//
-//        System.out.println(hashMap);
+        Store store = new Store(10000, "bill.txt");
+        store.sellDevice(person1, device1);
+
+
     }
 }
---
+++ src/main/java/Task/Store.java
@@ -0,0 +1,38 @@
+package Task;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+    Device device;
+    List<Device> deviceList = new ArrayList<>();
+    int balance;
+    FileWriter writer = new FileWriter("bill.txt");
+
+    public Store(int balance, String writer) throws IOException {
+        this.balance = balance;
+        this.writer = writer;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void sellDevice(Person person, Device device) throws IOException {
+        if (person.getMoney() < device.getPrice()) {
+            System.out.println("Вам не хватает денег для покупки.");
+        } else {
+            deviceList.remove(device);
+            person.setDevice(device);
+            person.setMoney(person.getMoney() - device.getPrice());
+            setBalance(balance + device.price);
+            writer.write(device.toString());
+        }
+    }
+}
